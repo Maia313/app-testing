65174fec078315817b0ee4fd97904e92c2437efc
Maia <28684401+Maia313@users.noreply.github.com>
2022-05-06 16:09:41 +0200

    Update Button.test.js

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Button from '../Button';
 
-describe("Button",function() {
+describe("Button", () => {
     let mountedButton;
 
     beforeEach(()=>{
@@ -58,7 +58,7 @@ describe("When location passed is undefined", ()=> {
         mountedButton = shallow(<Button {...props} />);
     });
 
-    it('displays the location', ()=> {
+    it('displays All Locations', ()=> {
         const locName = mountedButton.find('.location-button');
         expect(locName.text()).toEqual('All Locations');
     });
